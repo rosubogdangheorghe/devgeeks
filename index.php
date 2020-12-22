207bc1072d4f1a1c674998e6a13fbd9d35e5d267
rosubogdangheorghe <rosu.bogdan@gmail.com>
2020-12-22 14:36:32 +0200

    Final update

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $winner = Winner::showWinner($carl, $beast);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Carl and The Beast</title>
 </head>
 
 <body>
